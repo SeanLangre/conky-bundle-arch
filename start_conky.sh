89dd457c5e7e9fcb0a19cb7749405caea05c1db3
s <3351542+SeanLangre@users.noreply.github.com>
2026-03-21 17:51:49 +0100

    fixed

--- start_conky.sh
+++ start_conky.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sleep 10
 conky -c $HOME/.config/conky/stock/stock_indices.conkyrc --daemonize
+sleep 1
 conky -c $HOME/.config/conky/titus/system_conky.conkyrc --daemonize
+sleep 1
 
 # Start Twitch chat listener in background
 python3 $HOME/.config/conky/twitch_chat/twitch_chat.py &
 conky -c $HOME/.config/conky/twitch_chat/twitch_chat.conkyrc --daemonize
+sleep 1
 
 # Start Discord notification listener in background
 python3 $HOME/.config/conky/discord/discord_notifications.py &
