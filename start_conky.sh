be2e270ca5d1227ef924028200a4dcf452c58f03
s <3351542+SeanLangre@users.noreply.github.com>
2026-03-26 11:40:49 +0100

    archlinux

--- start_conky.sh
+++ start_conky.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
-sleep 10
-conky -c $HOME/.config/conky/stock/stock_indices.conkyrc --daemonize
-sleep 1
-conky -c $HOME/.config/conky/titus/system_conky.conkyrc --daemonize
+CONKY_DIR="$HOME/.config/conky"
+
+# Kill existing instances
+pkill -f twitch_chat.py
+pkill -f discord_notifications.py
+pkill conky
+sleep 2
+
+# stock
+conky -c "$CONKY_DIR/stock/stock_indices.conkyrc" --daemonize
 sleep 1
 
-# Start Twitch chat listener in background
-python3 $HOME/.config/conky/twitch_chat/twitch_chat.py &
-conky -c $HOME/.config/conky/twitch_chat/twitch_chat.conkyrc --daemonize
+# system
+conky -c "$CONKY_DIR/titus/system_conky.conkyrc" --daemonize
 sleep 1
 
-# Start Discord notification listener in background
-python3 $HOME/.config/conky/discord/discord_notifications.py &
-conky -c $HOME/.config/conky/discord/discord_notifications.conkyrc --daemonize
+# twitch
+python3 "$CONKY_DIR/twitch_chat/twitch_chat.py" &
+TWITCH_PY=$!
+conky -c "$CONKY_DIR/twitch_chat/twitch_chat.conkyrc" &
+TWITCH_CONKY=$!
+
+# discord
+python3 "$CONKY_DIR/discord/discord_notifications.py" &
+DISCORD_PY=$!
+conky -c "$CONKY_DIR/discord/discord_notifications.conkyrc" &
+DISCORD_CONKY=$!
+
+# Kill python when conky dies
+( wait $TWITCH_CONKY;  kill $TWITCH_PY  2>/dev/null ) &
+( wait $DISCORD_CONKY; kill $DISCORD_PY 2>/dev/null ) &
